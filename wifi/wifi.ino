7e89c920c15f5b511bf8d7d3655c9181eb797580
BangUl_Pro <wind.dong.dream@gmail.com>
2016-11-20 00:26:11 +0900

    Serial 통신

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -1,14 +1,23 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
+#include <Wire.h>
+
 
 #define SSID "paradise"
 #define PASS "tcm12345"
 
+SoftwareSerial toArduinoSerial(2, 3); // RX, TX
+
 ESP8266WebServer server(80);
 
 void setup(void) {
+  toArduinoSerial.begin(115200);
+  Serial.begin(115200);
+  Wire.begin();
   delay(1000);
-  Serial.begin(9600);
+  
+  toArduinoSerial.println("start");
   Serial.println("start");
   WiFi.begin(SSID, PASS);
 
@@ -38,24 +47,38 @@ void setup(void) {
     String musicIndex = server.arg("musicIndex");
     Serial.print("play "); 
     Serial.println(musicIndex);
+    toArduinoSerial.print("play "); 
+    toArduinoSerial.println(musicIndex);
+
+    Wire.beginTransmission(4);
+    Wire.write("play ");
+//    Wire.write(musicIndex);
+    Wire.endTransmission();
   });
 
   server.on("/stop", []()
   {
     server.send(200, "text/html", "stop");
     Serial.println("stop");
+    toArduinoSerial.println("stop");
+
+    Wire.beginTransmission(4);
+    Wire.write("stop");
+    Wire.endTransmission();
   });
 
   server.on("/next", []()
   {
     server.send(200, "text/html", "next");
     Serial.println("next");
+    toArduinoSerial.println("next");
   });
 
   server.on("/prev", []()
   {
     server.send(200, "text/html", "prev");
     Serial.println("prev");
+    toArduinoSerial.println("prev");
   });
 
   server.begin();
